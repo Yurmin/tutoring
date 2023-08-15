5a1b9769e68bf89a7f20f5eab02c394cf8a92d60
Yurmin <60743665+Yurmin@users.noreply.github.com>
2023-08-15 13:31:27 +0200

    Aggiunti timestamps alla classe `Operazione`

--- Banca/src/Banca/BancaTest.java
+++ Banca/src/Banca/BancaTest.java
@@ -1,14 +1,19 @@
 package Banca;
 
 public class BancaTest {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         ContoCorrente cc = new ContoCorrente(5000, "u1", "p1");
         try {
             cc.prelievo(6000);
+        } catch (ImportoMaggioreSaldoException e) {
+            e.printStackTrace();
+        } catch (ImportoNegativoException e) {
+            e.printStackTrace();
+        }
+        try {
             cc.deposito(-6000);
-        } catch (ImportoMaggioreSaldoException | ImportoNegativoException e) {
-            System.out.println("ImportoMaggioreSaldoException!");
+        } catch (ImportoNegativoException e) {
             e.printStackTrace();
         }
 
--- Banca/src/Banca/Operazione.java
+++ Banca/src/Banca/Operazione.java
@@ -1,5 +1,6 @@
 package Banca;
 
+import java.time.Instant;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,12 @@ public class Operazione {
     
     protected TipoOperazione tipo;
     protected UUID proprietario;
+    protected Instant time;
 
     public Operazione(TipoOperazione tipo, UUID proprietario) {
         this.tipo = tipo;
         this.proprietario = proprietario;
+        this.time = Instant.now();
     }
     
 }
